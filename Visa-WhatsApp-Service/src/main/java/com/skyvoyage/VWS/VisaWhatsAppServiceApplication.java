46819ec9feb299b255b6a7103b89eefa44c4b4ac
Faraaz <mohamedfaraaz007@gmail.com>
2024-11-26 13:19:20 +0530

    Initial commit

---
+++ Visa-WhatsApp-Service/src/main/java/com/skyvoyage/VWS/VisaWhatsAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.skyvoyage.VWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisaWhatsAppServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisaWhatsAppServiceApplication.class, args);
+	}
+
+}
